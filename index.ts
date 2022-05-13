af64367a27d8166cd24d913c3e45227ed1fb0dc3
florczak_arek <florczakarek@gmail.com>
2022-05-13 10:41:19 +0200

    add global error handling

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import 'express-async-errors';
+import { handleError, ValidationError } from './utils/error';
 
 const app = express();
 
@@ -10,6 +11,12 @@ app.use(
   })
 );
 app.use(express.json());
+
+app.get('/', async (req, res) => {
+  throw new ValidationError('Error jus came in');
+});
+
+app.use(handleError);
 
 app.listen(3001, '0.0.0.0', () => {
   console.log(`listening on port: http://localhost:3001`);
